340631408dbdbe8a05152810467a9ae6a6721915
manasa <manasa.mudham@gmail.com>
2023-03-27 14:00:53 +0000

    comit

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {name, imgUrl} = destinationDetails
+  return (
+    <li className="list-container">
+      <div className="list-item">
+        <img src={imgUrl} alt={name} className="list-image" />
+        <p className="para">{name}</p>
+      </div>
+    </li>
+  )
+}
+export default DestinationItem
